30f0be914ef86bdd084e442058e3e43f6e66ac95
Igor R. Guariroba <65466371+IgorGuariroba@users.noreply.github.com>
2023-06-08 04:50:31 +0000

    declaração

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+import styles from "./styles/Home.module.css";
+
 function Home(){
-    return <h1>teste1</h1>
+    return (
+        <div className={styles.container}>    
+          <main className={styles.main}>
+            <h1 className={styles.title}>
+              Minha querida,
+            </h1>
+    
+            <p className={styles.description}>
+              Desde que te conheci, meu coração não foi mais o mesmo. Você me preenche com alegria e amor a cada dia. Seu sorriso ilumina a minha vida e seu carinho me faz sentir completo. Eu amo você mais do que as palavras podem expressar. Obrigado por ser você.
+            </p>
+          </main>
+        </div>
+      );
 }
 
 export default Home;
