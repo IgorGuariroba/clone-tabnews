e668f127728a6ccb9f4a1d67c780beac90e758a6
igor.guariroba <igor.guariroba@jsl.com.br>
2023-12-06 17:41:49 -0300

    Adicionar campo 'updated_at' ao status da API
    Neste commit, foi adicionado o campo 'updated_at' à resposta do endpoint 'status' da API v1. Este campo representa a data e a hora em que o status foi verificado. Além disso, no teste de integração correspondente, foram adicionadas asserções para verificar a existência e a validade do campo 'updated_at'. Esta mudança foi feita para prover mais informações sobre o momento da verificação do status da API.

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,4 +2,9 @@ const axios = require("axios");
 test("GET to  /api/v1/status should return 200", async () => {
   const response = await axios.get("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  
+  const responseBody =  await  response.data;
+  expect(responseBody.updated_at).toBeDefined();
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
 });
