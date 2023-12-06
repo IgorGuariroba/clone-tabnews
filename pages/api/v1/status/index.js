7560c96fb8b9af24f0db945751bdee07b80d55c0
igor.guariroba <igor.guariroba@jsl.com.br>
2023-12-06 17:58:36 -0300

    Adiciona detalhes da conexão ao status da API
    Uma alteração foi feita para incluir mais informações na resposta da API de status. Agora, além da data e hora da última atualização, a resposta incluirá a versão do banco de dados, o número total de conexões e o número de conexões ativas. Essas informações adicionais fornecerão uma visão mais completa do estado da API e do banco de dados. Essa mudança aprimora a capacidade de monitoramento e depuração da API.
    
    A linha de console.log adicionada ao teste é para verificar se os dados estão sendo retornados conforme o esperado.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,21 @@
 import database from "infra/database";
+
 export default async function status(req, res) {
-  const updateAt = new Date().toISOString();
-  res.status(200).json({
-    updated_at: updateAt
-  });
+    const updateAt = new Date().toISOString();
+
+    const versionResult = await database.query("SELECT version()");
+    const databaseVersion = versionResult.rows[0].version;
+
+    const connectionsResult = await database.query("SELECT COUNT(*) FROM pg_stat_activity");
+    const connections = connectionsResult.rows[0].count;
+
+    const activeConnectionsResult = await database.query("SELECT COUNT(*) FROM pg_stat_activity WHERE state = 'active'");
+    const activeConnections = activeConnectionsResult.rows[0].count;
+
+    res.status(200).json({
+        updated_at: updateAt,
+        database_version: databaseVersion,
+        connections: connections,
+        active_connections: activeConnections
+    });
 }
