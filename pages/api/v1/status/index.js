31603559f72a8c2ccfe04f624bf5c2a079958676
igor.guariroba <igor.guariroba@jsl.com.br>
2023-12-06 16:28:03 -0300

    Atualiza o caminho do import e nome da função em status API
    O caminho para importar a base de dados foi atualizado ('../../../../infra/database' para 'infra/database') para melhorar a legibilidade e manutenção do código. Além disso, a função Index também foi renomeada para `status` para refletir melhor sua funcionalidade.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,5 @@
-import database from "../../../../infra/database";
-export default async function Index(req, res) {
+import database from "infra/database";
+export default async function status(req, res) {
   const result = await database.query("SELECT 1 + 1")
   console.log(result.rows)
   res.status(200).json({status: "OK"});
