de1b2c007257a7a6f4543fd40d3b56b0301a63af
Igor R. Guariroba <65466371+IgorGuariroba@users.noreply.github.com>
2024-03-22 18:57:00 +0000

    recomesando o projeto.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -19,3 +19,4 @@ export default async function status(req, res) {
         active_connections: activeConnections
     });
 }
+
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,11 +1,2 @@
-const axios = require("axios");
-test("GET to  /api/v1/status should return 200", async () => {
-  const response = await axios.get("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
-  
-  const responseBody =  await  response.data;
-  expect(responseBody.updated_at).toBeDefined();
-  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
-  console.log(response.data)
-});
+const response = await fetch("http://localhost:300/api/v1/status")
+expect(response.status).toBe(200)
