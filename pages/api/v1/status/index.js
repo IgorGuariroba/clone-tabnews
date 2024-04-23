b9fe7f6b3ecaeb9149438873146bcb77cd0ff819
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-04-23 16:51:12 -0300

    Ajustar o editor de idea

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,22 +1,22 @@
-import database from "infra/database";
+// import database from "infra/database";
 
-export default async function status(req, res) {
-    const updateAt = new Date().toISOString();
+// export default async function status(req, res) {
+//     const updateAt = new Date().toISOString();
 
-    const versionResult = await database.query("SELECT version()");
-    const databaseVersion = versionResult.rows[0].version;
+//     const versionResult = await database.query("SELECT version()");
+//     const databaseVersion = versionResult.rows[0].version;
 
-    const connectionsResult = await database.query("SELECT COUNT(*) FROM pg_stat_activity");
-    const connections = connectionsResult.rows[0].count;
+//     const connectionsResult = await database.query("SELECT COUNT(*) FROM pg_stat_activity");
+//     const connections = connectionsResult.rows[0].count;
 
-    const activeConnectionsResult = await database.query("SELECT COUNT(*) FROM pg_stat_activity WHERE state = 'active'");
-    const activeConnections = activeConnectionsResult.rows[0].count;
+//     const activeConnectionsResult = await database.query("SELECT COUNT(*) FROM pg_stat_activity WHERE state = 'active'");
+//     const activeConnections = activeConnectionsResult.rows[0].count;
 
-    res.status(200).json({
-        updated_at: updateAt,
-        database_version: databaseVersion,
-        connections: connections,
-        active_connections: activeConnections
-    });
-}
+//     res.status(200).json({
+//         updated_at: updateAt,
+//         database_version: databaseVersion,
+//         connections: connections,
+//         active_connections: activeConnections
+//     });
+// }
 
