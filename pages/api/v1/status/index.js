73982db2c22039aa35810420823a721beb4f2731
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-05-30 13:15:38 -0300

    Remove unnecessary console log from database connection and API status
    This commit removes unnecessary console log statements from the database connection file and the API status. By doing so, it enhances the overall cleanliness and readability of the code and reduces unnecessarily verbose output during the execution.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,6 +2,5 @@ import database from "infra/database";
 
 export default async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
   response.status(200).json({status: "está funcionando"});
 }
