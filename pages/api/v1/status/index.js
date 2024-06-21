ce288653edd4ee08ca2255554ddb541406e51e06
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-06-21 16:12:56 -0300

    "Ajustar endpoint de status e teste relacionado"
    Realizada a mudança na URL do teste de integração de "localhost" para "0.0.0.0". Além disso, atualizamos a key do objeto responseBody de "updated_at" para "updatedAt", também afetando a maneira como o tempo é tratado no teste. No endpoint de status, foi inserida uma consulta ao banco de dados para retornar a versão atual do banco, que agora também é verificada no teste de integração.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,9 +2,14 @@ import database from "infra/database";
 
 export default async function status(request, response) {
   const updatedAt = new Date().toISOString();
-  database.query("");
-
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
   response.status(200).json({
     updatedAt: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+      }
+    }
   });
 }
