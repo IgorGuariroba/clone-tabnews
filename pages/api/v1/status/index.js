7a80f22599c986b3ba6c2dc493b653995bb3972b
Igor R. Guariroba <65466371+igorguariroba@users.noreply.github.com>
2024-06-23 12:09:16 -0300

    "Refatorar consulta ao banco de dados para uso de variável de ambiente"
    A consulta à base de dados foi refatorada para usar a variável de ambiente POSTGRES_DB ao invés de um nome fixo. Isso aumenta a flexibilidade do código, permitindo que ele seja utilizado em diferentes ambientes e contextos.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -8,7 +8,11 @@ export default async function status(request, response) {
   const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
   const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
 
-  const databaseOpendConnectionsResult = await database.query("SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = 'nomedb';");
+  const databaseName = process.env.POSTGRES_DB
+  const databaseOpendConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  });
   const databaseOpenedConnectsValues = databaseOpendConnectionsResult.rows[0].count;
 
   response.status(200).json({
