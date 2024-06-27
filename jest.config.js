6dabcbe0f42db249d51574ec8aed280872e0d1d1
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-06-27 16:19:07 -0300

    Atualiza configurações do Jest para usar o next/jest
    Este commit atualiza a configuração do Jest no arquivo jest.config.js para utilizar o módulo next/jest. Também são adicionadas novas configurações para diretórios de módulos e arquivos de configuração inicial do Jest.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
-  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-};
+const nextJest = require("next/jest");
+
+const createdJestConfig = nextJest();
+const jestConfig = createdJestConfig({
+  moduleDirectories: ["node_modules","<rootDir>"],
+  setupFiles: ['<rootDir>/jest.setup.js'],
+});
+
+module.exports = jestConfig;
