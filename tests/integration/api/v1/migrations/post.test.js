373b3a7826f970e84713f076a6d1af9bc4b9acbe
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-06-27 17:19:21 -0300

    Adiciona teste de POST e refatora função de migrações
    Este commit adiciona um novo teste de POST na suite de testes de integração para a rota de migrações. Além disso, a função que executa as migrações foi refatorada para evitar a duplicação de código, criando um objeto com as opções default das migrações.

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -20,4 +20,13 @@ test("POST to /api/v1/migrations should return 200", async () => {
   expect(firstItem).toHaveProperty('path');
   expect(firstItem).toHaveProperty('name');
   expect(firstItem).toHaveProperty('timestamp');
+
+  const response2 = await fetch("http://0.0.0.0:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response2.status).toBe(200);
+
+  const responseBody2 = await response2.json();
+  expect(Array.isArray(responseBody2)).toBe(true);
+  expect(responseBody2.length).toBe(0);
 });
