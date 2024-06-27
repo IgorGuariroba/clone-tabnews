052520fe79c188a3483332ad0cd61cd93f54fee8
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-06-27 17:02:47 -0300

    "Ajusta configuração do Jest e retorna migrações na API"
    Este commit realiza duas ações principais. Primeiro, altera a configuração do Jest para apontar para o diretório atual. Em segundo lugar, modifica a API de migrações para retornar as migrações existentes em vez de um array vazio. Além disso, acrescenta um passo de limpeza no banco de dados antes de todos os testes de integração.

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,4 +1,8 @@
 import database from "infra/database.js";
+beforeAll(cleanDatabase)
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
 
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://0.0.0.0:3000/api/v1/migrations");
@@ -6,4 +10,5 @@ test("GET to /api/v1/migrations should return 200", async () => {
   const responseBody = await response.json();
   expect(Array.isArray(responseBody)).toBe(true);
   expect(responseBody.length).toBeGreaterThan(0);
+
 });
