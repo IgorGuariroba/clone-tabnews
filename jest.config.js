6d1bbfc26733b3e7d7fd8a893575b8287a83b579
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-06-27 16:32:58 -0300

    "Adiciona diretório na configuração do Jest"
    Detalhe: O diretório de trabalho foi adicionado na função nextJest para aprimorar a configuração do Jest. Isso otimiza a localização dos arquivos de teste.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 const nextJest = require("next/jest");
 
-const createdJestConfig = nextJest();
+const createdJestConfig = nextJest({
+  dir: './',
+});
 const jestConfig = createdJestConfig({
   moduleDirectories: ["node_modules","<rootDir>"],
   setupFiles: ['<rootDir>/jest.setup.js'],
