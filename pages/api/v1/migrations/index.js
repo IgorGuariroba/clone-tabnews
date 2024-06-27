1ccf8c8c81126fce053b0bd32097b6c10a6e75ec
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-06-27 12:47:55 -0300

    "Adiciona suporte a POST request e teste de integração para /api/v1/migrations"
    Esse commit adiciona a funcionalidade de suportar uma solicitação POST para o endpoint /api/v1/migrations e também um teste de integração correspondente. Este commit também refina a funcionalidade de GET existente e adiciona validação do método de solicitação, respondendo com um erro 405 para métodos de solicitação inválidos.

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,13 +1,28 @@
-import migrationRunner from "node-pg-migrate"
-import {join} from 'node:path'
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+
 export default async function migrations(request, response) {
-  const migrations = await migrationRunner({
-    databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
-    dir: join("infra","migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  });
-  response.status(200).json([]);
+  if (request.method === "POST") {
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: false,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+    response.status(200).json([]);
+  } else if (request.method === "GET") {
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+    response.status(200).json([]);
+  } else {
+    response.status(405).json({ error: "Invalid request method" });
+  }
 }
