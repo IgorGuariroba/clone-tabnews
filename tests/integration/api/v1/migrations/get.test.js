bc85a158c0a656e08e98cdba30a38e29713a1113
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-06-27 16:18:51 -0300

    Adiciona importação de database e teste no arquivo get.test.js
    A atualização adiciona a importação da infraestrutura do database para o arquivo de teste get.test.js. Também foi adicionada uma nova asserção para garantir que a resposta seja um array contendo pelo menos um item.

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,7 +1,9 @@
+import database from "infra/database.js";
+
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://0.0.0.0:3000/api/v1/migrations");
   expect(response.status).toBe(200);
-
   const responseBody = await response.json();
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
