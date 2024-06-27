ce51b1c06c03ce534ea6f3b58508ec5361aee5a6
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-06-27 17:19:21 -0300

    Adiciona teste de POST e refatora função de migrações
    Este commit adiciona um novo teste de POST na suite de testes de integração para a rota de migrações. Além disso, a função que executa as migrações foi refatorada para evitar a duplicação de código, criando um objeto com as opções default das migrações.

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,28 +1,26 @@
 import migrationRunner from "node-pg-migrate";
-import { join } from "node:path";
+import {join} from "node:path";
 
 export default async function migrations(request, response) {
+  const defaultMigrationsOptions = {
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  }
+
   if (request.method === "POST") {
     const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: false,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
+      ...defaultMigrationsOptions,
+      dryRun: false
     });
     response.status(200).json(migrations);
   } else if (request.method === "GET") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    });
+    const migrations = await migrationRunner(defaultMigrationsOptions);
     response.status(200).json(migrations);
   } else {
-    response.status(405).json({ error: "Invalid request method" });
+    response.status(405).json({error: "Invalid request method"});
   }
 }
