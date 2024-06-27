e84ef395e9426a915993bb9436d7940940fa2aef
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-06-27 17:11:14 -0300

    Adiciona mais verificações no teste de post de migrações
    Este commit adiciona verificações adicionais no teste de integração para POST em /api/v1/migrations. Agora, além de verificar o status da resposta e se o corpo da resposta é um array, o teste também verifica a estrutura do primeiro item do array.

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,14 +1,23 @@
 import database from "infra/database.js";
+
 beforeAll(cleanDatabase)
+
 async function cleanDatabase() {
   await database.query("drop schema public cascade; create schema public;");
 }
+
 test("POST to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://0.0.0.0:3000/api/v1/migrations",{
+  const response = await fetch("http://0.0.0.0:3000/api/v1/migrations", {
     method: "POST",
   });
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
   expect(Array.isArray(responseBody)).toBe(true);
+
+  // Verifique se o primeiro item do array tem a estrutura correta
+  const firstItem = responseBody[0];
+  expect(firstItem).toHaveProperty('path');
+  expect(firstItem).toHaveProperty('name');
+  expect(firstItem).toHaveProperty('timestamp');
 });
