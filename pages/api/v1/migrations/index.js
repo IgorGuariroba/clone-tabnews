85f3bab26e73a3a2698dc90f612cbb00330e2bc7
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-07-02 18:10:01 -0300

    "Adicione controle de método e tratamento de erros na API de migrações"
    Este commit adiciona uma verificação para os métodos HTTP permitidos na função de migração e envolve a lógica principal em um bloco try/catch, seguido por uma finalização garantida do cliente de banco de dados em um bloco finally.

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,29 +2,40 @@ import migrationRunner from "node-pg-migrate";
 import {join} from "node:path";
 import database from "infra/database.js";
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-  const defaultMigrationsOptions = {
-    dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  }
-
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationsOptions,
-      dryRun: false
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" is not allowed`,
     });
+  }
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+    const defaultMigrationsOptions = {
+      dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    }
+
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationsOptions,
+        dryRun: false
+      });
+
+      const code = migratedMigrations.length > 0 ? 201 : 200;
+      response.status(code).json(migratedMigrations);
+    } else if (request.method === "GET") {
+      const pendMigrations = await migrationRunner(defaultMigrationsOptions);
+      response.status(200).json(pendMigrations);
+    }
+  }catch (error){
+    console.log(error);
+    throw error;
+  }finally {
     await dbClient.end();
-    const code = migratedMigrations.length > 0 ? 201 : 200;
-    response.status(code).json(migratedMigrations);
-  } else if (request.method === "GET") {
-    const pendMigrations = await migrationRunner(defaultMigrationsOptions);
-    await dbClient.end();
-    response.status(200).json(pendMigrations);
-  } else {
-    response.status(405).json({error: "Invalid request method"});
   }
 }
