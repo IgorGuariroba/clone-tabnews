b13726e0a33ef8da91ca28dc1cd3a45df371e4f4
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-07-21 03:28:35 -0300

    "Ajusta importação e condição de status em testes"
    Na parte de testes do código, ocorreram duas mudanças. Primeiro, corrigimos a importação do "orchestrator" no arquivo "setup.js", adicionando a extensão ".js". Além disso, no arquivo "orchestrator.js", ajustamos a condição de verificação de status do "fetchStatusPage" para disparar erro caso o status não seja 200, ao invés de quando seja 200.

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,5 +1,4 @@
 import retry from "async-retry"
-import {error} from "next/dist/build/output/log";
 async function waitForAllServices(){
   await waitForWebServices();
 
@@ -11,7 +10,7 @@ async function waitForAllServices(){
 
     async function fetchStatusPage(){
       const response = await fetch("http://localhost:3000/api/v1/status");
-      if(response.status === 200){
+      if(response.status !== 200){
         throw Error();
       }
     }
