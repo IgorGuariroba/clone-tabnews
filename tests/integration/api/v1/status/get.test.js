b135a4b81f2d9fe57f5ec9e779dc8e95b7a86275
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-07-21 12:58:15 -0300

    Atualiza versão do banco de dados no teste de status
    A atualização se refere ao teste de integração de status, no qual foi modificada a versão esperada do banco de dados de "18.0" para "16.0" no arquivo 'get.test.js'.

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -7,7 +7,7 @@ test("GET to /api/v1/status should return 200", async () => {
 
   const parsedUpdatedAt = new Date(responseBody.updatedAt).toISOString();
   expect(responseBody.updatedAt).toEqual(parsedUpdatedAt);
-  expect(responseBody.dependencies.database.version).toEqual("18.0");
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
   expect(responseBody.dependencies.database.max_connnections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
