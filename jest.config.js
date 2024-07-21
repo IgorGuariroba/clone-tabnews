ea6abcc7e4dc7bf818aec238a63c1813e42051ba
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-07-21 12:41:14 -0300

    "Adicionada configuração do dotenv ao jest.config.js"
    Adicionamos a configuração do dotenv ao nosso arquivo jest.config.js para que possamos acessar as variáveis de ambiente durante os testes. Também ajustamos o tempo de timeout dos testes e melhoramos a formatação do código.

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
 const nextJest = require("next/jest");
+const dotenv = require("dotenv");
 
-const createdJestConfig = nextJest({
-  dir: '.',
-});
-const jestConfig = createdJestConfig({
-  moduleDirectories: ["node_modules","<rootDir>"],
+dotenv.config({ path: ".env.development" });
+const createJestConfig = nextJest({ dir: "." });
+
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
   setupFilesAfterEnv: ["<rootDir>/tests/setup.js"],
-  testTimeout: 60000,
+  // testTimeout: 60000,
 });
 
 module.exports = jestConfig;
