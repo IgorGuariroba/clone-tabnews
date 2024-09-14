9e50ea5c0d18ba120126d258279e0ae3c428eda4
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-09-14 15:44:38 -0300

    refactor: organizar testes de integração
    Reestrutura os testes de integração para agrupar cenários em descrições contextuais.
    Isso melhora a legibilidade e facilita a compreensão do contexto de cada teste.

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,13 +1,17 @@
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://0.0.0.0:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Usuario anónimo", () => {
+    test("Consultar status da aplicação", async () => {
+      const response = await fetch("http://0.0.0.0:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  expect(responseBody.updatedAt).toBeDefined();
+      const responseBody = await response.json();
+      expect(responseBody.updatedAt).toBeDefined();
 
-  const parsedUpdatedAt = new Date(responseBody.updatedAt).toISOString();
-  expect(responseBody.updatedAt).toEqual(parsedUpdatedAt);
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.max_connnections).toEqual(100);
-  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+      const parsedUpdatedAt = new Date(responseBody.updatedAt).toISOString();
+      expect(responseBody.updatedAt).toEqual(parsedUpdatedAt);
+      expect(responseBody.dependencies.database.version).toEqual("16.0");
+      expect(responseBody.dependencies.database.max_connnections).toEqual(100);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+    });
+  });
 });
