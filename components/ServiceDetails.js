850147b298d785e3785acdd906c7a58997553c6d
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-11-28 14:43:39 -0300

    refactor: modularizar lógica de renderização de tabela
    Melhora a clareza do código ao separar a lógica de renderização em funções distintas.
    As funções `renderTableRow`, `renderInnerValues` e `renderServiceDetails` foram criadas.
    Essa modulação facilita a manutenção e a leitura do código.

--- components/ServiceDetails.js
+++ components/ServiceDetails.js
@@ -1,5 +1,32 @@
 import React from "react";
 
+function renderInnerValues(subValue) {
+  return (
+    <ul className="ml-4 list-disc">
+      {Object.entries(subValue).map(([innerKey, innerValue]) => (
+        <li key={innerKey}>
+          <strong>{innerKey}:</strong> {innerValue}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function renderTableRow(subKey, subValue, index) {
+  return (
+    <tr key={subKey} className={`bg-gray-${index % 2 === 0 ? "800" : "700"} hover:bg-gray-600`}>
+      <td className="border border-gray-600 px-4 py-2">{subKey}</td>
+      <td className="border border-gray-600 px-4 py-2">
+        {typeof subValue === "object" ? renderInnerValues(subValue) : <span className="text-gray-300">{subValue}</span>}
+      </td>
+    </tr>
+  );
+}
+
+function renderServiceDetails(serviceData) {
+  return Object.entries(serviceData).map(([subKey, subValue], index) => renderTableRow(subKey, subValue, index));
+}
+
 function ServiceDetails({ serviceData }) {
   return (
     <div className="mt-4 transition-all duration-300 ease-in-out">
@@ -10,26 +37,7 @@ function ServiceDetails({ serviceData }) {
             <th className="px-4 py-2 border border-gray-600">Informações</th>
           </tr>
         </thead>
-        <tbody>
-          {Object.entries(serviceData).map(([subKey, subValue], index) => (
-            <tr key={subKey} className={`bg-gray-${index % 2 === 0 ? "800" : "700"} hover:bg-gray-600`}>
-              <td className="border border-gray-600 px-4 py-2">{subKey}</td>
-              <td className="border border-gray-600 px-4 py-2">
-                {typeof subValue === "object" ? (
-                  <ul className="ml-4 list-disc">
-                    {Object.entries(subValue).map(([innerKey, innerValue]) => (
-                      <li key={innerKey}>
-                        <strong>{innerKey}:</strong> {innerValue}
-                      </li>
-                    ))}
-                  </ul>
-                ) : (
-                  <span className="text-gray-300">{subValue}</span>
-                )}
-              </td>
-            </tr>
-          ))}
-        </tbody>
+        <tbody>{renderServiceDetails(serviceData)}</tbody>
       </table>
     </div>
   );
