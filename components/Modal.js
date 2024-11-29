097e1aff20fa3c63d3339700f5222c2c8a7dc8e7
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-11-29 15:23:19 -0300

    feat: redesenhar componente Modal
    Refatora o componente Modal para melhorar a interface de usuário e a acessibilidade.
    Adiciona estilos responsivos, tornando o modal mais atraente e funcional.
    Inclui estruturas de entrada de rádio para que o usuário possa selecionar serviços
    de forma mais intuitiva e organizada.

--- components/Modal.js
+++ components/Modal.js
@@ -1,13 +1,52 @@
-import { useRenderObject } from "@hooks/useRenderObject";
-
 export function Modal({ data }) {
-  const renderObject = useRenderObject();
-
   return (
-    <div className="modal">
-      {data.map((item, index) => (
-        <div key={index}>{typeof item === "object" ? renderObject(item) : item}</div>
-      ))}
+    <div className="w-full">
+      <div
+        id="select-modal"
+        tabindex="-1"
+        aria-hidden="true"
+        className="overflow-y-auto overflow-x-hidden z-50 max-h-full w-full flex justify-center items-center"
+      >
+        <div className="p-4 w-full max-w-md max-h-full">
+          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div className="flex items-center justify-between   rounded-t dark:border-gray-600"></div>
+            <div className="p-4 md:p-5">
+              <p className="text-gray-500 dark:text-gray-400 mb-4">Serviços:</p>
+              <ul className="space-y-4 mb-4">
+                {Object.entries(data).map(([key, value]) => (
+                  <li key={key}>
+                    <input
+                      type="radio"
+                      id={`job-${key}`}
+                      name="job"
+                      value={`job-${key}`}
+                      className="hidden peer"
+                      required
+                    />
+                    <label
+                      htmlFor={`job-${key}`}
+                      className="inline-flex items-center justify-between w-full p-5 text-gray-900 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-500 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-900 hover:bg-gray-100 dark:text-white dark:bg-gray-600 dark:hover:bg-gray-500"
+                    >
+                      <div className="block">
+                        <div className="w-full text-lg font-semibold">{key}</div>
+                        <div className="w-full text-gray-500 dark:text-gray-400">
+                          {typeof value === "object"
+                            ? Object.entries(value).map(([key, value], index) => (
+                                <p key={index} className="mb-2 text-gray-500 dark:text-gray-400">
+                                  <span className="font-bold capitalize">{key}:</span> {value}
+                                </p>
+                              ))
+                            : ""}
+                        </div>
+                      </div>
+                    </label>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
