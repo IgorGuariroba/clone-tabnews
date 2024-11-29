774334b63d701495afa1e09a9d7f62ebcfa04b38
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-11-29 16:13:55 -0300

    feat: adicionar exibição de data/hora no status
    Inclui um novo elemento que exibe a data e hora atuais na página de status.
    Ajusta o layout para manter uma interface consistente durante o carregamento e erros.
    Melhora a usabilidade ao fornecer contexto temporal aos usuários.

--- pages/status/index.js
+++ pages/status/index.js
@@ -45,11 +45,24 @@ export default function StatusPage() {
   const { data, error, isLoading } = useStatusData("/api/v1/status");
   const [viewType, setViewType] = useState("Accordion");
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error loading data</div>;
+  if (isLoading)
+    return <div className="min-h-screen bg-gray-900 p-10 text-white flex flex-col items-center">Loading...</div>;
+  if (error)
+    return (
+      <div className="min-h-screen bg-gray-900 p-10 text-white flex flex-col items-center">Error loading data</div>
+    );
 
   return (
     <div className="min-h-screen bg-gray-900 p-10 text-white flex flex-col items-center">
+      <div className="absolute top-0  left-0 p-4">
+        <div className="relative bg-gray-800 text-white p-6 rounded-lg shadow-lg max-w-sm">
+          <div className="flex flex-col items-center justify-center">
+            <span className="text-4xl font-bold tracking-wide">{new Date().toLocaleTimeString()}</span>
+            <span className="text-lg text-gray-400">{new Date().toLocaleDateString()}</span>
+          </div>
+        </div>
+      </div>
+
       <ViewSelector viewType={viewType} setViewType={setViewType} />
       <ViewContent viewType={viewType} data={data} />
     </div>
