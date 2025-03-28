bdfe3fbf2be30404db7c3e21b3b2b559eb3546fd
igor.guariroba <igor.guariroba@jsl.com.br>
2025-03-28 16:13:15 -0300

    feat: add runPendingMigrations function to orchestrator for migration handling

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,30 +1,36 @@
 import retry from "async-retry";
 import database from "../infra/database";
+import migrator from "models/migrator.js";
 
 async function waitForAllServices() {
-  await waitForWebServices();
+    await waitForWebServices();
 
-  async function waitForWebServices() {
-    return retry(fetchStatusPage, {
-      retries: 100,
-      maxTimeout: 1000,
-    });
+    async function waitForWebServices() {
+        return retry(fetchStatusPage, {
+            retries: 100,
+            maxTimeout: 1000,
+        });
 
-    async function fetchStatusPage() {
-      const response = await fetch("http://localhost:3000/api/v1/status");
-      if (response.status !== 200) {
-        throw Error();
-      }
+        async function fetchStatusPage() {
+            const response = await fetch("http://localhost:3000/api/v1/status");
+            if (response.status !== 200) {
+                throw Error();
+            }
+        }
     }
-  }
 }
 
 async function clearDatabase() {
-  await database.query("drop schema public cascade; create schema public;");
+    await database.query("drop schema public cascade; create schema public;");
+}
+
+async function runPendingMigrations() {
+    await migrator.runPendingMigrations();
 }
 
 const orchestrators = {
-  waitForAllServices,
-  clearDatabase,
+    waitForAllServices,
+    clearDatabase,
+    runPendingMigrations
 };
 export default orchestrators;
