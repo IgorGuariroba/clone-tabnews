49a6dd0bca4bb7aa9f415767ab3e6595719202f8
igor.guariroba <igor.guariroba@jsl.com.br>
2025-03-29 16:19:50 -0300

    feat: update user table schema with improved constraints and comments

--- infra/migrations/1743172822319_create-users.js
+++ infra/migrations/1743172822319_create-users.js
@@ -5,35 +5,40 @@ exports.up = (pgm) => {
             primaryKey: true,
             default: pgm.func("gen_random_uuid()"),
         },
-        // For referencie, GitHub username max length is 39 characters.
-        username: { 
+
+        // For reference, GitHub limits usernames to 39 characters.
+        username: {
             type: "varchar(30)",
             notNull: true,
-            unique: true 
+            unique: true,
         },
-        // Why 254 in length?  https://stackoverflow.com/a/1199238
-        email: { 
+
+        // Why 254 in length? https://stackoverflow.com/a/1199238
+        email: {
             type: "varchar(254)",
             notNull: true,
-            unique: true 
+            unique: true,
         },
-        // Why 254 in legth? https://security.stackexchange.com/a/39851
-        password: { 
-            type: "varchar(254)", 
-            notNull: true 
+
+        // Why 60 in length? https://www.npmjs.com/package/bcrypt#hash-info
+        password: {
+            type: "varchar(60)",
+            notNull: true,
         },
-        createdAt: {
+
+        // Why timestamp with timezone? https://justatheory.com/2012/04/postgres-use-timestamptz/
+        created_at: {
             type: "timestamptz",
             notNull: true,
-            default: pgm.func("current_timestamp"),
+            default: pgm.func("timezone('utc', now())"),
         },
-        // updatedAt: {
-        //     type: "timestamp",
-        //     notNull: true,
-        //     default: pgm.func("current_timestamp"),
-        // },
+
+        updated_at: {
+            type: "timestamptz",
+            notNull: true,
+            default: pgm.func("timezone('utc', now())"),
+        },
     });
 };
-
 
 exports.down = false;
