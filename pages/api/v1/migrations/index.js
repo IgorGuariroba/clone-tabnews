6445edf95e4532b9794453f0f23405fbb58ec9ad
igor.guariroba <igor.guariroba@jsl.com.br>
2025-03-29 16:20:23 -0300

    feat: enhance error handling with new ValidationError and NotFoundError classes

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,52 +1,25 @@
-import { createRouter } from "next-connect";
-import migrationRunner from "node-pg-migrate";
-import { resolve } from "node:path";
-import database from "infra/database.js";
-import controller from "../../../../infra/controller";
+import {createRouter} from "next-connect";
+import controller from "infra/controller.js";
+import migrator from "models/migrator.js";
 
 const router = createRouter();
 
-async function withDatabaseClient(callback) {
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
-    return await callback(dbClient);
-  } catch (error) {
-    console.log(error);
-    throw error;
-  } finally {
-    await dbClient.end();
-  }
-}
+router.get(getHandler);
+router.post(postHandler);
 
-const defaultMigrationsOptions = {
-  dryRun: true,
-  dir: resolve("infra", "migrations"),
-  direction: "up",
-  verbose: true,
-  migrationsTable: "pgmigrations",
-};
+export default router.handler(controller.errorHandlers);
 
 async function getHandler(request, response) {
-  await withDatabaseClient(async (dbClient) => {
-    const pendMigrations = await migrationRunner({ ...defaultMigrationsOptions, dbClient });
-    response.status(200).json(pendMigrations);
-  });
+    const pendingMigrations = await migrator.listPendingMigrations();
+    return response.status(200).json(pendingMigrations);
 }
 
 async function postHandler(request, response) {
-  await withDatabaseClient(async (dbClient) => {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationsOptions,
-      dbClient,
-      dryRun: false,
-    });
+    const migratedMigrations = await migrator.runPendingMigrations();
 
-    const code = migratedMigrations.length > 0 ? 201 : 200;
-    response.status(code).json(migratedMigrations);
-  });
+    if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+    }
+
+    return response.status(200).json(migratedMigrations);
 }
-
-router.get(getHandler).post(postHandler);
-
-export default router.handler(controller.errorHandlers);
